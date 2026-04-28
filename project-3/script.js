f86082a937078056ad590b14fc23299aecf26ea3
mwelsey <mwelsey@usc.edu>
2026-04-28 16:45:47 -0700

    fix

--- project-3/script.js
+++ project-3/script.js
@@ -1,13 +1,17 @@
 const elements = document.querySelectorAll(".fade");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-    }
-  });
-}, {
-  threshold: 0.25
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.25
+  }
+);
 
-elements.forEach(el => observer.observe(el));
+elements.forEach((el) => observer.observe(el));
+
